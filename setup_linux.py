ad78af9c43f0b8c0284b47cf395a78cb1e86d5fd
builderdev212 <builderdev212@gmail.com>
2024-12-20 10:46:12 -0500

    format setup scripts

--- setup_linux.py
+++ setup_linux.py
@@ -7,8 +7,11 @@ executables = [cx_Freeze.Executable("main.py", base=base)]
 cx_Freeze.setup(
     name="Racing Fire",
     version="1.0.1",
-    options={"build_exe": {"packages":["pygame", "time", "random", "pickle", "sys"],
-                           "include_files":["music/", "data/", "textures/"],
-    }},
-    executables = executables
+    options={
+        "build_exe": {
+            "packages": ["pygame", "time", "random", "pickle", "sys"],
+            "include_files": ["music/", "data/", "textures/"],
+        }
+    },
+    executables=executables,
 )
--- setup_windows.py
+++ setup_windows.py
@@ -5,33 +5,37 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 shortcut_table = [
-    ("DesktopShortcut",        # Shortcut
-     "DesktopFolder",          # Directory_
-     "Racing Fire",           # Name
-     "TARGETDIR",              # Component_
-     "[TARGETDIR]main.exe",# Target
-     None,                     # Arguments
-     None,                     # Description
-     None,                     # Hotkey
-     None,                     # Icon
-     None,                     # IconIndex
-     None,                     # ShowCmd
-     'TARGETDIR'               # WkDir
-),]
+    (
+        "DesktopShortcut",  # Shortcut
+        "DesktopFolder",  # Directory_
+        "Racing Fire",  # Name
+        "TARGETDIR",  # Component_
+        "[TARGETDIR]main.exe",  # Target
+        None,  # Arguments
+        None,  # Description
+        None,  # Hotkey
+        None,  # Icon
+        None,  # IconIndex
+        None,  # ShowCmd
+        "TARGETDIR",  # WkDir
+    ),
+]
 
 msi_data = {"Shortcut": shortcut_table}
-bdist_msi_options = {'data': msi_data}
+bdist_msi_options = {"data": msi_data}
 
 executables = [cx_Freeze.Executable("main.py", base=base)]
 
 cx_Freeze.setup(
     name="Racing Fire",
     version="1.0.1",
-    options={"build_exe": {"packages":["pygame", "time", "random", "pickle", "sys"],
-                           "include_files":["music/", "data/", "textures/"],
-                           "include_msvcr": True,
+    options={
+        "build_exe": {
+            "packages": ["pygame", "time", "random", "pickle", "sys"],
+            "include_files": ["music/", "data/", "textures/"],
+            "include_msvcr": True,
+        },
+        "bdist_msi": bdist_msi_options,
     },
-    "bdist_msi": bdist_msi_options
-    },
-    executables = executables
+    executables=executables,
 )
